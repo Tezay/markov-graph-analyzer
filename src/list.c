af03b7c5093764cca948fcc09fb17ebde6f203d9
aylliotthe <eliot.masson@efrei.net>
2025-10-21 16:50:05 +0200

    list.c finit + graph.c finit

--- src/list.c
+++ src/list.c
@@ -1,10 +1,23 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h>
 #include "list.h"
 
+//**
+// * @brief Initialise une liste vide
+// * 
+// * @param l Pointeur vers la liste qui va être créer
+// */
 void list_inint(List *l) {
     l->head = NULL;
 }
 
+//**
+// * @brief  Ajoute un nouvel élément au début d'une liste chaînée
+// * 
+// * @param l      Pointeur vers la liste à modifier
+// * @param dest   Sommet de destination à stocker dans la nouvelle cellule
+// * @param proba  Probabilité associée à la transition vers `dest`
+// */
 void  list_push_front(List *l, int dest, float proba){
     Cell *new = malloc(sizeof(Cell));
     if (!new) {
@@ -17,11 +30,31 @@ void  list_push_front(List *l, int dest, float proba){
     l->head = new;
 }
 
+//**
+// * @brief  Affiche tous les éléments d'une liste chaînée
+// * 
+// * @param l  Liste à afficher
+// */
 void list_print(List l){
-    while(l != NULL){
+
+    while(l.head != NULL){
         printf("(%d, %f)", l.head->dest, l.head->proba);
         l.head = l.head->next;
     }
 }
 
-//TODO : list_free
+//**
+// * @brief  Libère toute la mémoire allouée pour une liste chaînée
+// * 
+// * @param l  Pointeur vers la liste à libérer
+// */
+void list_free(List *l) {
+    Cell *cur = l->head;
+    Cell *tmp;
+
+    while (cur != NULL) {
+        tmp = cur->next;
+        free(cur);
+        cur = tmp;
+    }
+}
